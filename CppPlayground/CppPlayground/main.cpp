db14e5a0c4d0c36d0528ce574b83e91e008e69f4
Hassan ElDesouky <hassaneldesouky@icloud.com>
2021-03-14 07:31:32 +0200

    Add project files.

---
+++ CppPlayground/CppPlayground/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  CppPlayground
+//
+//  Created by Hassan El Desouky on 9/29/20.
+//
+
+#include <iostream>
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Allocator.h"
+#include "llvm/Support/StringSaver.h"
+
+
+
+int main(int argc, const char * argv[]) {
+
+  
+  return 0;
+}
